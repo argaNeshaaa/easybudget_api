8e2d14255bbcb6e5d1a59c986b638431ca56535a
argaNeshaaa <arganesha2006@gmail.com>
2025-12-04 06:11:58 +0800

    Debugging Gemini Fix

--- config/gemini.js
+++ config/gemini.js
@@ -10,6 +10,6 @@ if (!process.env.GEMINI_API_KEY) {
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 // Ganti ke 'gemini-1.5-flash'
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
 
 export default model;
