e0ac14ce207bdf20b8f77a6370d70d22b0a41853
argaNeshaaa <arganesha2006@gmail.com>
2025-12-04 05:41:45 +0800

    Debugging AI

--- routes/ai.js
+++ routes/ai.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { verifyToken } from "../middleware/authMiddleware.js";
-import { chatController } from "../controllers/aiController.js";
+import { chatController } from "../controllers/aiControllers.js";
 
 const router = express.Router();
 
